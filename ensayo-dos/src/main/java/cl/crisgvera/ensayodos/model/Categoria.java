7b847d050310ea05335c5754643718f7df0609e8
Cristóbal Gajardo <cristobalgajardo.v@gmail.com>
2020-09-02 15:59:00 -0400

    readme ensayo-cuatro removed

--- ensayo-dos/src/main/java/cl/crisgvera/ensayodos/model/Categoria.java
+++ ensayo-dos/src/main/java/cl/crisgvera/ensayodos/model/Categoria.java
@@ -1,6 +1,5 @@
 package cl.crisgvera.ensayodos.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
